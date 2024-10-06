88669965bd96339137ed147cdca8d589a8ec06d4
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-10-06 21:22:39 +0530

    product conroller change

--- Controller/productController.js
+++ Controller/productController.js
@@ -51,11 +51,69 @@ export const findByIdProduct = async(req,res)=>{
 
 }
 export const updateProduct = async(req,res)=>{
+    try {
+        const id  = req.params.id;
+        const { name, description, image, unitprice, QoH } = req.body;
+
+
+        const updatedProducts = await productSchema.findByIdAndUpdate(
+            id,
+            { name, description, image, unitprice, QoH },
+            { new: true, runValidators: true } 
+        );
+
+        if (!updatedProducts) {
+            return res.status(404).json({ success: false, message: "products not found" });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Product updated successfully",
+            products: updatedProducts,
+        });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Server error", error: error.message });
+    }
 
 }
 export const deleteByIdProduct = async(req,res)=>{
+    try {
+        const id  = req.params.id;
+
+        
+
+        const deletedProducts = await productSchema.findByIdAndDelete(id);
+
+        if (!deletedProducts) {
+            return res.status(404).json({ success: false, message: "Product not found" });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Prodcut deleted successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Server error", error: error.message });
+    }
 
 }
 export const findAllProduct = async(req,res)=>{
+    try {
+        const products = await productSchema.find(); 
+        
+        if (!products.length) {
+            return res.status(404).json({ success: false, message: "No product found" });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Here are all the products",
+            products: products
+        });
+
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Server error", error: error.message });
+    }
+
 
 }
